3de93759a8964397a8a4d0e6ff6ef7e0ff4c8cce
Joshua Castaneda <joshua444jc@gmail.com>
2020-07-19 14:47:57 -0700

    Looks alright, but may need to fiddle with a bit more padding

--- src/commands/commandList/user/profile.js
+++ src/commands/commandList/user/profile.js
@@ -3,13 +3,13 @@ const Profile = require("../../../models/user/profile");
 
 const stats = { 
   "health" : ":heart:",
-  // "defence" : ":shield:",
+  "defence" : ":shield:",
   "strength" : ":crossed_swords:",
-  // "attack_speed" : ":dagger:",
-  // "gold_find" : ":moneybag:",
-  // "speed" : ":dash:",
-  // "luck": ":four_leaf_clover:",
-  // "xp_gain": ":star2:"
+  "attack_speed" : ":dagger:",
+  "gold_find" : ":moneybag:",
+  "speed" : ":dash:",
+  "luck": ":four_leaf_clover:",
+  "xp_gain": ":star2:"
 };
 
 module.exports = new CommandInterface({
@@ -25,39 +25,22 @@ async function showProfile(p){
 
   let pf = await Profile.get(p.user.id);
 
-  let embed = {
-    "color": 13679088,
-    "author": {
-      "name": p.user.username + "'s Profile",
-      "icon_url": p.user.avatarURL
-    },
-    "thumbnail": {
-      "url": p.user.avatarURL
-    },
-    "fields": [
-      {
-        "name": "Character",
-        "value":pf.helmet + blank + pf.weapon1 + nl +
-                pf.chestplate + blank + pf.weapon2 + nl +
-                pf.pants + blank + blank + nl +
-                blank + blank + blank + nl +
-                pf.accessory1 + pf.accessory2 + pf.accessory3
-      },
-      {
-        "name": "Stats",
-        "value":""
-      }
-      ]
-    };
+  let msg = 
+    blank + ":scroll: **" + p.user.username + "'s Profile**" + nl +
+    "__Character:__" + nl +
+    blank + pf.helmet + blank + pf.accessory1 + blank + "Tile: " + pf.tile + nl +
+    pf.weapon1 + pf.chestplate + pf.weapon2 + pf.accessory2 + blank + "Gold: " + pf.gold + nl +
+    blank + pf.pants + blank + pf.accessory3 + blank + "Experience: " + pf.xp + nl +
+    "__Stats:__" + nl;
 
   for(stat in stats){
-    if(i%2==0){
-      embed.fields[1].value += stats[stat] + " **" + pf[stat] + "** (+" + 0 + ") " + nl;
+    if(i%3==0){
+      msg += stats[stat] + " `" + pf[stat] + " (+" + 0 + ")`" + nl;
     } else {
-      embed.fields[1].value += stats[stat] + " **" + pf[stat] + "** (+" + 0 + ")" + blank;
+      msg += stats[stat] + " `" + pf[stat] + "(+" + 0 + ")` ";
     }
     i++;
   }
 
-  p.send({embed});
+  p.send(msg);
 }
--- src/models/user/profile.js
+++ src/models/user/profile.js
@@ -2,21 +2,23 @@ const ModelInterface = require("../ModelInterface");
 
 module.exports = new ModelInterface({
   _id: String,
-  helmet: {type: String, default: "<:hIcon:733868285409624176>"},
-  chestplate: {type: String, default: "<:cIcon:733867487199887381>"},
-  pants: {type: String, default: "<:pIcon:733866769961189488>"},
+  helmet: {type: String, default: "<:hIcon:732820031372656721>"},
+  chestplate: {type: String, default: "<:cIcon:732821083463614554>"},
+  pants: {type: String, default: "<:pIcon:732822688258588765>"},
   weapon1: {type: String, default: "<:wIcon:732825599319605259>"},
   weapon2: {type: String, default: "<:wIcon:732825599319605259>"},
   accessory1: {type: String, default: "<:rIcon:732828071346044960>"},
   accessory2: {type: String, default: "<:nIcon:732983438298316951>"},
   accessory3: {type: String, default: "<:aIcon:732981186712043600>"},
   xp: {type: Number, default: 0},
+  gold: {type: Number, default: 0},
+  tile: {type: String, default: "1.1"},
   health: {type: Number, default: 5},
-  // defence: {type: Number, default: 0},
+  defence: {type: Number, default: 0},
   strength: {type: Number, default: 2},
-  // attack_speed: {type: Number, default: 1},
-  // gold_find: {type: Number, default: 0},
-  // speed: {type: Number, default: 0},
-  // luck: {type: Number, default: 0},
-  // xp_gain: {type: Number, default: 0}
+  attack_speed: {type: Number, default: 1},
+  gold_find: {type: Number, default: 0},
+  speed: {type: Number, default: 0},
+  luck: {type: Number, default: 0},
+  xp_gain: {type: Number, default: 0}
 }, "profile", "profiles");
