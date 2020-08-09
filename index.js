7a8f23aae05f5755203b21547a466567fb7712b7
Joshua Castaneda <joshua444jc@gmail.com>
2020-08-08 20:27:19 -0700

    Created debug bot

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+
+const testing = true;
+
 const config = require("./src/data/config.json");
 
 const Sharder = require('eris-sharder').Master;
-const sharder = new Sharder(process.env.TOKEN, process.env.SHARDER_PATH, {
-    name: process.env.NAME,
+const sharder = new Sharder((testing) ? process.env.TEST_BOT_TOKEN : process.env.BOT_TOKEN, process.env.SHARDER_PATH, {
+    name: (testing) ? process.env.TEST_NAME : process.env.NAME,
     clientOptions: config.clientOptions
 }); //This starts the bot
---
+++ src/commands/commandList/exploration/map.js
@@ -0,0 +1,33 @@
+const CommandInterface = require("../../commandInterface");
+// const MapData = require("../../../data/map.json");
+
+module.exports = new CommandInterface({
+
+    alias:["map"],
+
+    usage: "{#}",
+
+    desc: "View the world map",
+
+    examples: ["map 2", "map"],
+
+    category: "Exploration",
+    
+    execute: async function(p){
+        let pf = await p.db.User.findById(p.sender.id) ?? await p.db.User.create({_id: p.sender.id});
+        let map = "";
+
+        for(i = 1; i<=100; i++){
+            if(pf.tile == i){
+                map += ":x:";
+            } else if(pf.bestTile >= i){
+                map += MapData.tileSet[MapData.map[i-1]].icon;
+            } else {
+                map += "▫️";
+            }
+            if(i%10==0){map += "\n";}
+        }
+
+        p.send(map);
+  }
+});
