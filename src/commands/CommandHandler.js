bdd86f02cf4a95895e3f8b046a14373669ec3b6b
Joshua Castaneda <joshua444jc@gmail.com>
2021-01-17 21:56:47 -0800

    Command handling

--- src/commands/CommandHandler.js
+++ src/commands/CommandHandler.js
@@ -6,17 +6,36 @@ const commands = {};
 module.exports = class CommandHandler{
 
 	constructor(client){
+		this.client = client;
 		registerCommands();
 	}
 	
-	async executeCheck(data){
+	async executeCheck(interaction){
+		const {token, member, data, channel_id} = interaction;
 		const {id} = data
 
-        if(!commands[id]) return;
+		if(!commands[id]) return;
+		
+		let params = getParams(this.client, interaction);
 
-        await commands[id].execute(data);
+        await commands[id].execute(params);
 	}
 
+}
+
+function getParams(client, interaction){
+	const param = {
+		client: client,
+		interaction: interaction,
+		reply: async (reply) => {
+			return await client.rest.createInteractionResponse(interaction, reply);
+		},
+		send: async (message) => {
+			return await client.rest.createMessage(interaction.channel_id, message).then((msg) => new client.Message(msg, client));
+		}
+	}
+
+	return param;
 }
 
 async function registerCommands(){
--- src/commands/CommandInterface.js
+++ src/commands/CommandInterface.js
@@ -3,6 +3,7 @@ module.exports = class CommandInterface{
 	constructor(args){
         this.id = args.id;
         this.cooldown = args.cooldown;
+        this.syntax = args.syntax
         this.execute = args.execute;
     }
 
