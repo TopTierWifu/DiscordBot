767a40ee6f5fa2c1d4d5a61fe527f16e4b4e1f9e
Joshua Castaneda <joshua444jc@gmail.com>
2021-01-18 23:32:30 -0800

    Cleanup

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ const TOKEN = require("./secret.json").TOKEN;
 
 const BOT = new Client(TOKEN);
 BOT.login();
-
-// async function test(){
-//     let test = await BOT.rest.getUser("210177401064390658");
-//     console.log(test);
-// }
-
-// test();
-
-// BOT.on("ready", () => {
-//     console.log("Bot has logged in");
-// });
