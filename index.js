c7bf0835f328f55f89f2386199c5ff4550d7d202
Joshua Castaneda <joshua444jc@gmail.com>
2021-05-01 13:38:01 -0700

    Fixed Eris options

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Master = require("eris-sharder").Master;
 const { token } = require("../tokens/bot-auth.json");
 
 const sharder = new Master(token, "/src/bot.js", {
-    allowedMentions: {
-        everyone: false,
-        roles: false,
-        repliedUser: false
-    },
-    defaultImageFormat: "png",
-    intents: 771,
-    messageLimit: 1,
     stats: true,
-    name: "Bot"
+    name: "Bot",
+    clientOptions: {
+        defaultImageFormat: "png",
+        defaultImageSize: 1024,
+        intents: 771,
+        messageLimit: 0,
+        allowedMentions: {
+            everyone: false,
+            roles: false,
+            repliedUser: false
+        }
+    }
 });
