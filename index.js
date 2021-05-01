61b78242682d317413516bda1e8905b0b1635fc2
Joshua Castaneda <joshua444jc@gmail.com>
2021-05-01 15:17:55 -0700

    Fixed allowed mentions

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const { token } = require("../tokens/bot-auth.json");
 const sharder = new Master(token, "/src/bot.js", {
     stats: true,
     name: "Bot",
+    allowedMentions: {
+        everyone: false,
+        roles: false,
+        repliedUser: false,
+        user: true
+    },
     clientOptions: {
         defaultImageFormat: "png",
         defaultImageSize: 1024,
         intents: 771,
         messageLimit: 0,
         restMode: true,
-        allowedMentions: {
-            everyone: false,
-            roles: false,
-            repliedUser: false
-        }
     }
 });
