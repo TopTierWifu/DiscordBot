582d45f9293939dd7695ba6ca8120952a4aea31f
Joshua Castaneda <joshua444jc@gmail.com>
2021-05-01 20:33:37 -0700

    Making it easier to register slash commands for both bots

--- slashCommand.js
+++ slashCommand.js
@@ -4,9 +4,13 @@ const debug = true;
  * @typedef {Omit<import("./src/commands/command").ApplicationCommand, "application_id" | "id">} Command
  */
 const fetch = require("node-fetch").default;
-const { token } = require("../tokens/bot-auth.json");
+const token = debug ? require("../tokens/bot-auth.json").testToken : require("../tokens/bot-auth.json").token;
 const botID = debug ? "741854059748786176" : "539465213246963724";
+
 const testGuildID = "604719438805205004";
+const testCommandID = "798081847266770945";
+const wifuID = "210177401064390658";
+
 const discordURL = "https://discord.com/api/v8";
 
 /**@type {Command} */
@@ -29,39 +33,77 @@ const json = {
  * @param {"GET" | "POST" | "PUT" | "DELETE" | "PATCH"} method 
  * @param {*} [body] 
  */
-function request(route, method = "GET", body) {
+async function request(route, method = "GET", body) {
     return fetch(`${discordURL}${route}`, { method, body, headers: { 'Content-Type': 'application/json', Authorization: `Bot ${token}` } })
-    .then(res => res.json());
+    .then(async res => {
+        try {
+            return await res.json();
+        } catch (error) {
+            return res;
+        }
+    });
 }
+
+/*
+ * GUILD COMMANDS
+ */
+
+/**
+ * @param {string} guildId
+ * @param {*} body 
+ */
+async function createGuildCommand(guildId, body) {
+    return await request(`/applications//${botID}/guilds/${guildId}/commands`, "POST", JSON.stringify(body));
+}
+
+/**
+ * @param {string} commandId 
+ * @param {string} guildId
+ * @param {*} body
+ */
+async function editGuildCommand(commandId, guildId, body) {
+    return await request(`/applications/${botID}/guilds/${guildId}/commands/${commandId}`, "PATCH", JSON.stringify(body));
+}
+
+/**
+ * @param {string} commandId 
+ * @param {string} guildId
+ */
+async function deleteGuildCommand(commandId, guildId) {
+    return await request(`/applications/${botID}/guilds/${guildId}/commands/${commandId}`, "DELETE");
+}
+
+/**
+ * @param {string} guildId 
+ */
+async function getGuildCommands(guildId) {
+    return await request(`/applications/${botID}/guilds/${guildId}/commands`)
+}
+
+
+
+/*
+ * GLOBAL COMMANDS
+ */
 
 /**
  * @param {*} body 
  */
-async function createGuildCommand(body) {
-    return await request(`/applications//${botID}/guilds/${testGuildID}/commands`, "POST", JSON.stringify(body));
-}
-
-/**
- * @param {string} commandId 
- * @param {*} body
- */
-async function editGuildCommand(commandId, body) {
-    return await request(`/applications/${botID}/guilds/${testGuildID}/commands/${commandId}`, "PATCH", JSON.stringify(body));
-}
-
-/**
- * @param {string} commandId 
- */
-async function deleteGuildCommand(commandId) {
-    return await request(`/applications/${botID}/guilds/${testGuildID}/commands/${commandId}`, "DELETE");
-}
-
-async function getGuildCommands() {
-    return await request(`/applications/${botID}/guilds/${testGuildID}/commands`)
+async function createGlobalCommand(body) {
+    return await request(`/applications/${botID}/commands`, "POST", JSON.stringify(body));
 }
 
 async function getGlobalCommands() {
     return await request(`/applications/${botID}/commands`);
 }
 
-getGlobalCommands().then(res => console.log(res));
+
+/*
+ * TEST COMMAND
+ */
+
+async function editTestCommand(body) {
+    editGuildCommand(testCommandID, testGuildID, body);
+}
+
+// deleteGuildCommand("838125469387194438", testGuildID).then(res => console.log(res));
